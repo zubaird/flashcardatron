6f32dd38c2934864576db04f9d38c3f3315b6010
Zubair Desai <zubair.desai@gmail.com>
2015-02-26 20:01:20 -0800

    Update broc and install package for heroku deployment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
+import ENV from '../config/environment';
 
 export default DS.ActiveModelAdapter.extend({
-  host:"http://localhost:3000"
+  // host:"http://localhost:3000"
+  host: ENV.adapterURL || "http://localhost:3000"
 });
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    adapterURL: process.env.ADAPTER_URL,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
